24739e66ee0b7433404fd1e5017dea99d8e20640
plug <plug185@gmail.com>
2024-07-27 00:51:39 +0700

    Add: Guards and validate pipe

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidateInputPipe } from './core/pipe/validate.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // global prefix
   app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(new ValidateInputPipe())
   await app.listen(3000);
 }
 bootstrap();
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,15 +1,37 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
 import { scrypt as _scrypt, randomBytes } from 'crypto';
 import { promisify } from 'util';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
+import { UserDto } from '../users/dto/user.dto';
 
 const scrypt = promisify(_scrypt);
 @Injectable()
 export class AuthService {
     constructor(private usersService: UsersService, private jwtService: JwtService) { }
-    async register(email: string, pass: string) {
+    async register(data: UserDto) {
+        // Check if email exists should return
+        const userExists = this.usersService.findOneByEmail(data.email);
+        if(userExists){
+            throw new BadRequestException('email in use');
+        }
+        // Generate salt
         const salt = randomBytes(8).toString('hex')
+
+        // Hash password
+        const hash = (await scrypt(data.password, salt, 32)) as Buffer;
+
+        const passwordHashed = salt + '.' + hash.toString('hex');
+
+        // Store to database
+        const newUser = await this.usersService.create({ ...data, password: passwordHashed});
+        const { password, ...result } = newUser['dataValues'];
+
+        // generate token
+        const payload = { email: data.email, sub: newUser.id };
+        const token = await this.generateToken(payload)
+
+        return { user: result, accessToken: token}
 
     }
 
@@ -28,10 +50,14 @@ export class AuthService {
     }
 
     async login(user: any) {
-        const payload = { email: user.email, sub: user.userId };
+        const payload = { email: user.email, sub: user.id };
         return {
-            access_token: this.jwtService.sign(payload),
+            access_token: this.generateToken(payload),
         };
+    }
+
+    private async generateToken(payload){
+        return await this.jwtService.signAsync(payload);
     }
 
   
