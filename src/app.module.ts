dbb818b0fa1576451c28b25bab3cb3248ed6f392
plug <plug185@gmail.com>
2024-07-27 12:55:51 +0700

    Create: Post resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { DatabaseModule } from './core/database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { PostModule } from './modules/post/post.module';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true}), 
